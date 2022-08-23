0a5d8ba156e79407c60972476a7c950ddb13cb33
Edinorock <111507622+Edinorock@users.noreply.github.com>
2022-08-24 02:18:49 +0300

    Сделано ДЗ

---
+++ src/Main.java
@@ -0,0 +1,90 @@
+public class Main {
+
+    public static void main (String [] args) {
+        // Задача №1
+        System.out.println("Задача 1");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        // Задача №2
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        // Задача №3
+        System.out.println("Задача 3");
+        var dog1 = 8.0;
+        dog1 = dog1 - 3.5;
+        System.out.println(dog1);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        // Задача №4
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        // Задача №5
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = (frog / 3.5) + 4;
+        System.out.println(frog);
+        // Задача №6
+        System.out.println("Задача 6");
+        var boxerWeight = 78.2;
+        var boxerWeight2 = 82.7;
+        System.out.println(boxerWeight + boxerWeight2);
+        System.out.println(boxerWeight2 - boxerWeight);
+        // Задача №7
+        System.out.println("Задача 7");
+        System.out.println(boxerWeight2 - boxerWeight);
+        System.out.println(boxerWeight2 % boxerWeight);
+        // Задача №8 часть 1
+        System.out.println("Задача 8 часть 1");
+        var workingHours = 640;
+        var workingHoursOneEmployee = 8;
+        var howManyEmployees = workingHours / workingHoursOneEmployee;
+        System.out.println("Всего работников в компании – " + howManyEmployees + " человек");
+        // Задача №8 часть 2
+        System.out.println("Задача 8 часть 2");
+        var howManyEmployees1 = workingHours / workingHoursOneEmployee + 94;
+        var workingHours1 = howManyEmployees1 * 8;
+        System.out.println("Если в компании работает " + howManyEmployees1 + " человека, то всего " + workingHours1 + " часа работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
